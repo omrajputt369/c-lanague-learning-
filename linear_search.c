926cbed29cb349e12421f910c16b18dbfa57f882
Om Rajput <omrajputt369@gmail.com>
2026-02-11 22:05:51 +0530

    Add linear_search.c

---
+++ linear_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  int n, i, search, found = 0;
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+  int arr[n];
+
+  printf("Enter %d elements:\n", n);
+  for (i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  printf("Enter element to search: ");
+  scanf("%d", &search);
+
+  for (i = 0; i < n; i++) {
+    if (arr[i] == search) {
+      printf("%d is present at location %d.\n", search, i + 1);
+      found = 1;
+      break;
+    }
+  }
+
+  if (!found)
+    printf("%d is not present in the array.\n", search);
+
+  return 0;
+}
